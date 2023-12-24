48b9b31d0e2843eb7fd52900895a2f026c1bacaf
Mark Shaffer <31354065+CodeMelted@users.noreply.github.com>
2023-12-24 18:41:56 -0500

    Fix title inconsistencies.

--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['codemelted_20embedded_20module_0',['CodeMelted Embedded Module',['../index.html',1,'']]]
+  ['codemelted_20_2d_20embedded_20module_0',['CodeMelted - Embedded Module',['../index.html',1,'']]]
 ];
